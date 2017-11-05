56fa8b5ff23b308fdab220ece2dc7e3f1080cb75
胡栋松 <hudongsong@meituan.com>
2017-11-05 12:37:56 +0800

    Init Commit

---
+++ LearningOpenGL/LearningOpenGL/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LearningOpenGL
+//
+//  Created by yihuds on 2017/11/5.
+//  Copyright © 2017年 hudongsong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
